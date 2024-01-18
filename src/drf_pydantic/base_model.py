99b97a7abbf9f195dbd2726f3a1aeb737a1f4aff
George Bocharov <georgeme007@gmail.com>
2024-01-18 06:15:46 +0000

    1.0.0 release; added type hints for base model

--- src/drf_pydantic/base_model.py
+++ src/drf_pydantic/base_model.py
@@ -3,10 +3,12 @@ import typing
 import pydantic
 
 from rest_framework import serializers
+from typing_extensions import dataclass_transform
 
 from drf_pydantic.parse import create_serializer_from_model
 
 
+@dataclass_transform(kw_only_default=True, field_specifiers=(pydantic.Field,))
 class ModelMetaclass(pydantic.main.ModelMetaclass, type):
     def __new__(mcs, name, bases, namespace, **kwargs):
         cls = super().__new__(mcs, name, bases, namespace, **kwargs)
@@ -17,4 +19,4 @@ class ModelMetaclass(pydantic.main.ModelMetaclass, type):
 class BaseModel(pydantic.BaseModel, metaclass=ModelMetaclass):
     if typing.TYPE_CHECKING:
         # populated by the metaclass, defined here to help IDEs only
-        drf_serializer: type[serializers.Serializer]
+        drf_serializer: typing.ClassVar[type[serializers.Serializer]]
