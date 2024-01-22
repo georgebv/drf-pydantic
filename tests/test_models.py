82a57262b4bd5d46ce95f3ebdb8fc6fff2c2b3a2
George Bocharov <george@kicksaw.com>
2024-01-22 05:03:35 +0000

    refactored model tests, fixed nested model bugs

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,18 +1,13 @@
 import datetime
 import typing
 
-from enum import Enum
-from unittest.mock import patch
-
 import pydantic
-import pytest
 
 from drf_pydantic import BaseModel
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
 
-def test_simple_model():
+def test_model_with_multiple_fields():
     class Person(BaseModel):
         name: str
         email: pydantic.EmailStr
@@ -21,11 +16,14 @@ def test_simple_model():
         address: typing.Optional[str]
         date_of_birth: datetime.date
         nationality: str = "USA"
+        friends: list[str]
 
     serializer = Person.drf_serializer()
 
     assert serializer.__class__.__name__ == "PersonSerializer"
-    assert len(serializer.fields) == 7
+    assert len(serializer.fields) == 8
+
+    field: serializers.Field
 
     # Regular fields
     assert isinstance(serializer.fields["name"], serializers.CharField)
@@ -33,99 +31,35 @@ def test_simple_model():
     assert isinstance(serializer.fields["age"], serializers.IntegerField)
     assert isinstance(serializer.fields["height"], serializers.FloatField)
     assert isinstance(serializer.fields["date_of_birth"], serializers.DateField)
-    for name in ["name", "email", "age", "height", "date_of_birth"]:
+    assert isinstance(serializer.fields["friends"], serializers.ListField)
+    for name in ["name", "email", "age", "height", "date_of_birth", "friends"]:
         field = serializer.fields[name]
-        assert field.required is True, name
-        assert field.default is serializers.empty, name
-        assert field.allow_null is False, name
+        assert field.required is True
+        assert field.default is serializers.empty
+        assert field.allow_null is False
 
     # Optional
-    field: serializers.Field = serializer.fields["address"]
-    assert isinstance(field, serializers.CharField)
-    assert field.allow_null is True
-    assert field.default is None
-    assert field.required is False
+    optional_field = serializer.fields["address"]
+    assert isinstance(optional_field, serializers.CharField)
+    assert optional_field.required is True
+    assert optional_field.default is serializers.empty
+    assert optional_field.allow_null is True
 
     # With default
-    field: serializers.Field = serializer.fields["nationality"]
-    assert isinstance(field, serializers.CharField)
-    assert field.allow_null is False
-    assert field.default == "USA"
-    assert field.required is False
+    field_with_default = serializer.fields["nationality"]
+    assert isinstance(field_with_default, serializers.CharField)
+    assert field_with_default.required is False
+    assert field_with_default.default == "USA"
+    assert field_with_default.allow_null is False
 
 
-def test_model_with_literal():
-    class Human(BaseModel):
-        sex: typing.Literal["male", "female"]
-        age: int
-
-    serializer = Human.drf_serializer()
-
-    assert serializer.__class__.__name__ == "HumanSerializer"
-    assert len(serializer.fields) == 2
-
-    # Normal field
-    assert isinstance(serializer.fields["age"], serializers.IntegerField)
-    assert not isinstance(serializer.fields["age"], serializers.CharField)
-
-    # Literal field
-    field: serializers.Field = serializer.fields["sex"]
-    assert not isinstance(field, serializers.CharField)
-    assert isinstance(field, serializers.ChoiceField)
-    assert set(field.choices) == set(["male", "female"])
-
-
-def test_model_with_list():
-    class Team(BaseModel):
-        name: str
-        value: typing.Optional[float]
-        members: list[str]
-
-    serializer = Team.drf_serializer()
-
-    assert serializer.__class__.__name__ == "TeamSerializer"
-    assert len(serializer.fields) == 3
-
-    # Normal fields
-    assert isinstance(serializer.fields["name"], serializers.CharField)
-    assert isinstance(serializer.fields["value"], serializers.FloatField)
-
-    # List field
-    field = serializer.fields["members"]
-    assert isinstance(field, serializers.ListField)
-    assert isinstance(field.child, serializers.CharField)
-    assert field.default is serializers.empty
-    assert field.allow_empty is True
-
-
-def test_iheritance_future():
-    class Person(pydantic.BaseModel):
-        name: str
-        age: int
-
-    class Employee(BaseModel, Person):
-        title: typing.Literal["grunt", "boss"]
-        salary: float
-
-    serializer = Employee.drf_serializer()
-
-    assert serializer.__class__.__name__ == "EmployeeSerializer"
-    assert len(serializer.fields) == 4
-
-    assert isinstance(serializer.fields["name"], serializers.CharField)
-    assert isinstance(serializer.fields["age"], serializers.IntegerField)
-    assert isinstance(serializer.fields["salary"], serializers.FloatField)
-    assert isinstance(serializer.fields["title"], serializers.ChoiceField)
-    assert set(serializer.fields["title"].choices) == set(["boss", "grunt"])
-
-
-def test_inheritance_past():
+def test_inheritance():
     class Person(BaseModel, pydantic.BaseModel):
         name: str
         age: int
 
     class Employee(Person):
-        title: typing.Literal["grunt", "boss"]
+        title: str
         salary: float
 
     serializer = Employee.drf_serializer()
@@ -136,13 +70,32 @@ def test_inheritance_past():
     assert isinstance(serializer.fields["name"], serializers.CharField)
     assert isinstance(serializer.fields["age"], serializers.IntegerField)
     assert isinstance(serializer.fields["salary"], serializers.FloatField)
-    assert isinstance(serializer.fields["title"], serializers.ChoiceField)
-    assert set(serializer.fields["title"].choices) == set(["boss", "grunt"])
+    assert isinstance(serializer.fields["title"], serializers.CharField)
+
+
+def test_iheritance_from_non_drf_model():
+    class Person(pydantic.BaseModel):
+        name: str
+        age: int
+
+    class Employee(BaseModel, Person):
+        title: str
+        salary: float
+
+    serializer = Employee.drf_serializer()
+
+    assert serializer.__class__.__name__ == "EmployeeSerializer"
+    assert len(serializer.fields) == 4
+
+    assert isinstance(serializer.fields["name"], serializers.CharField)
+    assert isinstance(serializer.fields["age"], serializers.IntegerField)
+    assert isinstance(serializer.fields["salary"], serializers.FloatField)
+    assert isinstance(serializer.fields["title"], serializers.CharField)
 
 
 def test_nested_model():
     class Job(BaseModel):
-        title: typing.Literal["grunt", "boss"]
+        title: str
         salary: float
 
     class Person(BaseModel):
@@ -151,7 +104,7 @@ def test_nested_model():
 
     serializer = Person.drf_serializer()
 
-    # Top model
+    # Parent model
     assert serializer.__class__.__name__ == "PersonSerializer"
     assert len(serializer.fields) == 2
     assert isinstance(serializer.fields["name"], serializers.CharField)
@@ -161,11 +114,11 @@ def test_nested_model():
     job: serializers.Serializer = serializer.fields["job"]
     assert job.__class__.__name__ == "JobSerializer"
     assert len(job.fields) == 2
-    assert isinstance(job.fields["title"], serializers.ChoiceField)
+    assert isinstance(job.fields["title"], serializers.CharField)
     assert isinstance(job.fields["salary"], serializers.FloatField)
 
 
-def test_nested_model_list():
+def test_list_of_nested_models():
     class Apartment(BaseModel):
         floor: int
         owner: str
@@ -176,7 +129,7 @@ def test_nested_model_list():
 
     serializer = Building.drf_serializer()
 
-    # Top model
+    # Parent model
     assert serializer.__class__.__name__ == "BuildingSerializer"
     assert len(serializer.fields) == 2
     assert isinstance(serializer.fields["address"], serializers.CharField)
@@ -190,7 +143,7 @@ def test_nested_model_list():
     assert isinstance(apartment.fields["owner"], serializers.CharField)
 
 
-def test_nested_model_only_last():
+def test_non_drf_nested_model():
     class Apartment(pydantic.BaseModel):
         floor: int
         owner: str
@@ -201,7 +154,7 @@ def test_nested_model_only_last():
 
     serializer = Building.drf_serializer()
 
-    # Top model
+    # Parent model
     assert serializer.__class__.__name__ == "BuildingSerializer"
     assert len(serializer.fields) == 2
     assert isinstance(serializer.fields["address"], serializers.CharField)
@@ -213,101 +166,3 @@ def test_nested_model_only_last():
     assert len(apartment.fields) == 2
     assert isinstance(apartment.fields["floor"], serializers.IntegerField)
     assert isinstance(apartment.fields["owner"], serializers.CharField)
-
-
-def test_int_constraint():
-    with pytest.warns(UserWarning) as warninfo:
-
-        class Person(BaseModel):
-            name: str
-            email: pydantic.EmailStr
-            age: int = pydantic.Field(ge=0, le=100)
-            salary: int = pydantic.Field(gt=0, lt=100)
-
-        assert all("not supported by DRF" in str(wrn.message) for wrn in warninfo)
-
-    serializer = Person.drf_serializer()
-
-    assert isinstance(serializer.fields["age"], serializers.IntegerField)
-
-    # With constraints
-    field: serializers.Field = serializer.fields["age"]
-    assert isinstance(field, serializers.IntegerField)
-    assert field.allow_null is False
-    assert field.required is True
-    assert field.min_value == 0
-    assert field.max_value == 100
-
-    # With constraints
-    field: serializers.Field = serializer.fields["salary"]
-    assert isinstance(field, serializers.IntegerField)
-    assert field.allow_null is False
-    assert field.required is True
-    assert field.min_value == 0
-    assert field.max_value == 100
-
-
-def test_str_constraint():
-    class Person(BaseModel):
-        name: str = pydantic.Field(min_length=3, max_length=10)
-        email: pydantic.EmailStr
-        age: int
-
-    serializer = Person.drf_serializer()
-
-    assert isinstance(serializer.fields["name"], serializers.CharField)
-
-    field: serializers.Field = serializer.fields["name"]
-    assert isinstance(field, serializers.CharField)
-    assert field.allow_null is False
-    assert field.required is True
-    assert field.min_length == 3
-    assert field.max_length == 10
-
-
-def test_model_with_list_from_typing():
-    class Item(BaseModel):
-        name: str
-
-    class Cart(BaseModel):
-        items: typing.List[Item]
-
-    serializer = Cart.drf_serializer()
-
-    assert isinstance(serializer, serializers.Serializer)
-
-    items_field: serializers.Field = serializer.fields["items"]
-    assert isinstance(items_field, serializers.ListField)
-    assert items_field.child.__class__.__name__ == "ItemSerializer"
-
-    name_field: serializers.Field = items_field.child.fields["name"]
-    assert isinstance(name_field, serializers.CharField)
-
-
-def test_enum_value():
-    class SexEnum(Enum):
-        MALE = "male"
-        FEMALE = "female"
-        OTHER = "other"
-
-    class Human(BaseModel):
-        sex: SexEnum
-        age: int
-
-    serializer = Human.drf_serializer
-
-    normal_serializer = serializer(data={"sex": SexEnum.MALE, "age": 25})
-
-    assert normal_serializer.is_valid()
-    assert normal_serializer.validated_data["sex"] == SexEnum.MALE
-    assert normal_serializer.validated_data["age"] == 25
-
-    value_serializer = serializer(data={"sex": "male", "age": 25})
-
-    assert value_serializer.is_valid()
-    assert value_serializer.validated_data["sex"] == SexEnum.MALE
-    assert value_serializer.validated_data["age"] == 25
-
-    bad_value_serializer = serializer(data={"sex": "bad_value", "age": 25})
-
-    assert bad_value_serializer.is_valid() is False
