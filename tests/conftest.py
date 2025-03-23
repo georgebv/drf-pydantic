1efeeb79bc51c1e3e86a41a7927faaeeded3e3a1
georgebv <georgeme007@gmail.com>
2025-03-23 03:45:37 +0000

    configure django to enable validation in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
+import django
 import pytest
+
+from django.conf import settings
 
 from drf_pydantic.parse import SERIALIZER_REGISTRY
 
@@ -7,3 +10,26 @@ from drf_pydantic.parse import SERIALIZER_REGISTRY
 def reset_serializers():
     SERIALIZER_REGISTRY.clear()
     assert len(SERIALIZER_REGISTRY) == 0
+
+
+@pytest.fixture(autouse=True, scope="session")
+def django_setup():
+    """Required to run serializer.is_valid()"""
+    if not settings.configured:
+        settings.configure(
+            DEBUG=True,
+            SECRET_KEY="fake-key",  # noqa
+            INSTALLED_APPS=[
+                "django.contrib.auth",
+                "django.contrib.contenttypes",
+                "rest_framework",
+            ],
+            DATABASES={
+                "default": {
+                    "ENGINE": "django.db.backends.sqlite3",
+                    "NAME": ":memory:",
+                }
+            },
+            MIDDLEWARE=[],
+        )
+    django.setup()
