79802a43cd051e26d4d5a5bb7ef1c3d33c4074c3
georgebv <georgeme007@gmail.com>
2025-03-23 06:40:30 +0000

    format pydantic error

--- src/drf_pydantic/base_serializer.py
+++ src/drf_pydantic/base_serializer.py
@@ -1,8 +1,11 @@
+import json
+
 from typing import TYPE_CHECKING, Any, Type, TypeVar
 
 import pydantic
 
 from rest_framework import serializers  # type: ignore
+from rest_framework.settings import api_settings  # type: ignore
 
 if TYPE_CHECKING:
     from drf_pydantic.base_model import BaseModel
@@ -22,11 +25,44 @@ class DrfPydanticSerializer(serializers.Serializer):
             try:
                 self._pydantic_model(**attrs)
             except pydantic.ValidationError as exc:
-                if self._pydantic_model.drf_config.get("validation_error") == "drf":
-                    raise serializers.ValidationError(exc) from exc
-                assert (
+                if (
                     self._pydantic_model.drf_config.get("validation_error")
                     == "pydantic"
-                )
-                raise exc
+                ):
+                    raise exc
+                assert self._pydantic_model.drf_config.get("validation_error") == "drf"
+
+                field_errors: dict[str, list[str]] = {}
+                non_field_errors: list[str] = []
+                for error in exc.errors():
+                    try:
+                        message = json.dumps(
+                            {
+                                "loc": error["loc"],
+                                "msg": error["msg"],
+                                "type": error["type"],
+                            }
+                        )
+                    except:  # noqa
+                        message = f"{error['msg']} (type={error['type']})"
+                    if (
+                        len(error["loc"]) == 0
+                        or not isinstance(error["loc"][0], str)
+                        or error["loc"][0] not in self.fields
+                    ):
+                        non_field_errors.append(message)
+                    else:
+                        field_errors.setdefault(error["loc"][0], []).append(message)
+
+                raise serializers.ValidationError(
+                    {
+                        getattr(
+                            api_settings,
+                            "NON_FIELD_ERRORS_KEY",
+                            "non_field_errors",
+                        ): non_field_errors,
+                        **field_errors,
+                    }
+                ) from exc
+
         return return_value
