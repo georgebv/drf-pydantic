1ba4417f806faeb4eeedea9e4dec6409957d88d3
George Bocharov <georgeme007@gmail.com>
2025-12-13 21:08:32 -0800

    Bump version from 2.9.0 to 2.9.1

--- src/drf_pydantic/__init__.py
+++ src/drf_pydantic/__init__.py
@@ -2,7 +2,7 @@ __all__ = [
     "BaseModel",
     "DrfPydanticSerializer",
 ]
-__version__ = "2.9.0"
+__version__ = "2.9.1"
 
 from drf_pydantic.base_model import BaseModel
 from drf_pydantic.base_serializer import DrfPydanticSerializer
